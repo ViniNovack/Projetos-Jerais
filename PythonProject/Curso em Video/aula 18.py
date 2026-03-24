514b8a4a9baafdff7a6866b1ddb4f47c5fa82adc
ViniNovack <vininomn@gmail.com>
2026-03-24 08:38:11 -0300

    Atualização 1.2.5

--- PythonProject/Curso em Video/aula 18.py
+++ PythonProject/Curso em Video/aula 18.py
@@ -15,21 +15,21 @@
 #     print(f'O/A {p[0]} tem {p[1]} anos de idade! \n')
 
 # EXEMPLO 3
-galera = []
-dado = []
-totmal = 0
-tormen = 0
-for c in range(0, 3):
-    dado.append(str(input('Nome: ')))
-    dado.append(int(input('Idade: ')))
-    galera.append(dado[:])
-    dado.clear()
-
-for p in galera:
-    if p in galera:
-        print(f'{p[0]} é maior de idade! \n')
-        totmal +=1
-    else:
-        print(f'{p[0]} é menor de idade! \n')
-        totmen +=1
-print(f'Temos {totmal} maior e {totmen} menor de idade! \n')
+# galera = []
+# dado = []
+# totmal = 0
+# totmen = 0
+# for c in range(0, 3):
+#     dado.append(str(input('Nome: ')))
+#     dado.append(int(input('Idade: ')))
+#     galera.append(dado[:])
+#     dado.clear()
+#
+# for p in galera:
+#     if p[1] >= 21:
+#         print(f'{p[0]} é maior de idade! \n')
+#         totmal +=1
+#     else:
+#         print(f'{p[0]} é menor de idade! \n')
+#         totmen +=1
+# print(f'Temos {totmal} maior e {totmen} menor de idade! \n')
