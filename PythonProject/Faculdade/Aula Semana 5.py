2be9cf76267bbc081319caf4b82b75c1589f1b24
ViniNovack <vininomn@gmail.com>
2026-03-26 16:08:21 -0300

    Atualização Aula 5 FACULDADE

---
+++ PythonProject/Faculdade/Aula Semana 5.py
@@ -0,0 +1,8 @@
+#EXERCICIO 3
+n = int(input('Digite um número: \n'))
+
+x = 0
+while x <= 10:
+    r = x * n
+    print(f'{x} * {n} = {r}')
+    x +=1
---
+++ PythonProject/Faculdade/Exercicios Semana 5.py
@@ -0,0 +1,36 @@
+#EXERCICIO 1
+# while True:
+#     n = int(input('Digite um valor: \n'))
+#     if 0 <= n <= 10:
+#         print(f'O valor {n} é VALIDO \n')
+#         break
+#     else:
+#         print(f'O valor {n} é INVALIDO')
+# print('=-' *30)
+# print('FIM')
+
+#EXERCICIO 2
+L = []
+while True:
+    n = str(input('Digite números (digte "sair" parafinalizar o codigi): \n'))
+    if n.lower() == 'sair':
+        break
+    else:
+        L.append(int(n))
+
+s = str(input('Escolha um sinal entre "+" ou "-" \n'))
+
+y = 0
+x = 0
+
+if s == '+':
+    for c in L:
+        x = x + c
+elif s == '-':
+    for m in L:
+        y = y + m
+
+if s == '+':
+    print(f'O resultado da "+" é {x} \n')
+elif s == '-':
+    print(f'O resultado da "-" é {y} \n')
