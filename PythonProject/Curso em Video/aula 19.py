a07a3ad68b06958040b45585408810f47f563c6f
ViniNovack <vininomn@gmail.com>
2026-03-29 11:21:53 -0300

    Atualização Curso em video aula 19, Exemplos

--- PythonProject/Curso em Video/aula 19.py
+++ PythonProject/Curso em Video/aula 19.py
@@ -1,5 +1,22 @@
 #EXEMPLO 1
-dados = {}
-dados = {'nome': 'Pedro', 'idade': 25}
-dados ['sexo'] = 'M'
-print(dados['sexo'])
+# dados = {}
+# dados = {'nome': 'Pedro', 'idade': 25}
+# dados ['sexo'] = 'M'
+# print(dados['sexo'])
+
+#EXEMPLO 2
+# filme = {'titulo': 'star wars', 'ano': 1977, 'diretor': 'George Lucas'}
+# print(filme.values())
+# print(filme.keys())
+# print(filme.items())
+# for k, v in filme.items():
+#     print(f'O {k} é {v}')
+
+#EXEMPLO 3
+# filme1 = {'titulo': 'star wars', 'ano': '1977', 'diretor': 'George Lucas'}
+# filme2 = {'titulo': 'avengers', 'ano': '2012', 'diretor': 'Joss Whedon'}
+# filme3 = {'titulo': 'matrix', 'ano': '1999', 'diretor': 'Wachowski'}
+# locadora = [filme1, filme2, filme3]
+# print(locadora[0]['titulo'])
+
+#EXEMPLO 4
