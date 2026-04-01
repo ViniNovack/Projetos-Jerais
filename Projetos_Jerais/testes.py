37af3e4272e350b241c229c25341e4f052c5fa90
ViniNovack <vininomn@gmail.com>
2026-04-01 09:01:23 -0300

    Atualização Curso em vídeo Exercicio 93

--- Projetos_Jerais/testes.py
+++ Projetos_Jerais/testes.py
@@ -1,3 +1,9 @@
-L = [[1,'v'], [8,'f'], [3,'g']]
-L.sort()
-print(L)
+#Testes exercicio 91
+# L = [[1,'v'], [8,'f'], [3,'g']]
+# L.sort()
+# print(L)
+
+#Testes exercicio 92
+# L = [1 ,2, 3, 4, 5]
+# total = sum(L)
+# print(total)
